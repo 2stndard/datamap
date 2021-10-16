b35eb390b3ef0314d66d50a442b0edc39f0d4bac
unknown <2stndard@gmail.com>
2021-10-17 00:46:19 +0900

    211022-2203

--- datamap.R
+++ datamap.R
@@ -1,14 +1,14 @@
 library(readxl)
 library(tidyverse)
-install.packages('clipr')
+## install.packages('clipr')
 library(clipr)
 
-df <- read_excel('D:/Work/2021/데이터맵/연구데이터 조사표(최종).xlsx', skip = 2, na = '-', sheet = '통합', col_types = c(rep('text',26)), col_names = F)
+df <- read_excel('./연구데이터 조사표(최종).xlsx', skip = 2, na = '-', sheet = '통합', col_types = c(rep('text',26)), col_names = F)
 
 
 colnames(df) <- c('기관명', '과제명', '연구책임자', '조사명', '조사설명', '분석분야', '분석분야_기타', '분석대상', '분석대상_기타', '조사방법', '조사방법_기타', '설문대상', '설문대상_기타', '설문규모', '설문규모_기타', '설문지공개', '설문지공개_기타', '조사회수', '조사회수_기타', '응답자기본정보항목수', '응답자기본정보항목수_기타', '원본데이터포함여부', '원본데이터포함여부_기타', '다년조사', '다년조사_기타', '승인통계여부', '승인통계여부_기타')
 
-distinct(df, 분석분야)
+distinct(df, 설문대상)
 
 
 
@@ -27,6 +27,10 @@ df$다년조사 <- as.factor(df$다년조사)
 df$승인통계여부 <- as.factor(df$승인통계여부)
 
 df$분석분야 <- fct_relevel(df$분석분야, '교육전반', '유아분야', '초중등교육', '고등교육', '평생교육', '기타')
+
+df$설문규모 <- fct_relevel(df$설문규모, '~19', '20~99', '100~499', '500~999', '1000~')
+
+df$설문대상 <- fct_relevel(df$설문대상, '대국민', '정부부처, 교육청', '연구자 및 전문가', '학교행정가(교장, 교감, 보직교원 등)', '교원', '학생', '학부모', '전문가, 행정가 및 교원', '기타')
 
 ##  기관별 과제명
 df |> 
@@ -154,7 +158,13 @@ df |>
   ungroup()|>
   ggplot(aes(x = 분석분야, y = 조사방법)) +
   geom_text(aes(label = n)) + 
-  facet_wrap(~기관명)
+  facet_wrap(~기관명, ncol = 1) +
+  scale_y_discrete(labels = c("만족도조사","면담 및 FGI","설문조사", '패널조사', '협의및토론\n델파이'))
+
+
+ggsave("plot1.png", width = 13.5, height = 17.5, units = "cm")
+
+?facet_wrap
 
 ## 
 df |> 
@@ -163,8 +173,9 @@ df |>
   ungroup()|>
   ggplot(aes(x = 분석분야, y = 분석대상)) +
   geom_text(aes(label = n)) + 
-  facet_wrap(~기관명)
+  facet_wrap(~기관명, ncol = 1)
 
+ggsave("plot2.png", width = 13.5, height = 17.5, units = "cm")
 
 ## 
 df |> 
@@ -172,9 +183,10 @@ df |>
   count() |>
   ungroup()|>
   ggplot(aes(x = 분석분야, y = 설문대상)) +
-  geom_text(aes(label = n)) + 
-  facet_wrap(~기관명)
+  geom_text(aes(label = n), size = 3) + 
+  facet_wrap(~기관명, ncol = 1)
 
+ggsave("plot3.png", width = 13.5, height = 17.5, units = "cm")
 
 ## 
 df |> 
@@ -182,8 +194,11 @@ df |>
   count() |>
   ungroup()|>
   ggplot(aes(x = 조사방법, y = 설문대상)) +
-  geom_text(aes(label = n)) + 
-  facet_wrap(~기관명)
+  geom_text(aes(label = n), size = 3) + 
+  facet_wrap(~기관명, ncol = 1)+ 
+  scale_x_discrete(labels = c("만족도조사","면담 및 FGI","설문조사", '패널조사', '협의및토론\n델파이'))
+
+ggsave("plot4.png", width = 13.5, height = 17.5, units = "cm")
 
 
 ## 
@@ -193,7 +208,10 @@ df |>
   ungroup()|>
   ggplot(aes(x = 조사방법, y = 설문규모)) +
   geom_text(aes(label = n)) + 
-  facet_wrap(~기관명)
+  facet_wrap(~기관명, ncol = 1) + 
+  scale_x_discrete(labels = c("만족도조사","면담 및 FGI","설문조사", '패널조사', '협의및토론\n델파이'))
+
+ggsave("plot5.png", width = 13.5, height = 17.5, units = "cm")
 
 
 ## 
@@ -203,14 +221,20 @@ df |>
   ungroup()|>
   ggplot(aes(x = 설문대상, y = 설문규모)) +
   geom_text(aes(label = n)) + 
-  facet_wrap(~기관명)
+  facet_wrap(~기관명, ncol = 1) + 
+  scale_x_discrete(labels = c("대국민","정부부처\n교원","연구자\n전문가", '학교행정가', '교원', '학생', '학부모', '전문가\n행정가\n교원', '기타'))
+
+ggsave("plot6.png", width = 13.5, height = 17.5, units = "cm")
 
 
 ## 
 df |> 
-  group_by(기관명, 분석분야, 설문규모) |>
+  group_by(설문대상, 분석분야, 설문규모) |>
   count() |>
   ungroup()|>
   ggplot(aes(x = 분석분야, y = 설문규모)) +
   geom_text(aes(label = n)) + 
-  facet_wrap(~기관명)
+  facet_wrap(~설문대상, ncol = 2) + 
+  scale_x_discrete(labels = c("교육\n전반","유아\n분야","초중등\n교육", '고등\n교육', '평생\n교육', '기타'))
+
+ggsave("plot7.png", width = 13.5, height = 17.5, units = "cm")
