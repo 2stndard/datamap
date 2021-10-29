a12b4f6bcf69ad6b00d1d8d3eb4678380ec7ea73
이기준 <2stndard@gmail.com>
2021-10-29 22:07:17 +0900

    211029-2207

--- datamap.R
+++ datamap.R
@@ -155,11 +155,13 @@ df |>
 df |> 
   group_by(기관명, 분석분야, 조사방법) |>
   count() |>
-  ungroup()|>
-  ggplot(aes(x = 분석분야, y = 조사방법)) +
-  geom_text(aes(label = n)) + 
+  group_by(기관명) |>
+  mutate(nn = sum(n)) |>   ggplot(aes(x = 분석분야, y = 조사방법)) +
+   geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = nn)) +
+  geom_text(aes(label = n), color = 'white') + 
   facet_wrap(~기관명, ncol = 1) +
-  scale_y_discrete(labels = c("만족도조사","면담 및 FGI","설문조사", '패널조사', '협의및토론\n델파이'))
+  scale_y_discrete(labels = c("만족도조사","면담 및 FGI","설문조사", '패널조사', '협의및토론\n델파이')) +
+  labs(fill = '전체사례수')
 
 
 ggsave("plot1.png", width = 13.5, height = 17.5, units = "cm")
@@ -170,10 +172,13 @@ ggsave("plot1.png", width = 13.5, height = 17.5, units = "cm")
 df |> 
   group_by(기관명, 분석분야, 분석대상) |>
   count() |>
-  ungroup()|>
-  ggplot(aes(x = 분석분야, y = 분석대상)) +
-  geom_text(aes(label = n)) + 
-  facet_wrap(~기관명, ncol = 1)
+  group_by(기관명) |>
+  mutate(nn = sum(n)) |>   ggplot(aes(x = 분석분야, y = 분석대상)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = nn)) +
+  geom_text(aes(label = n), color = 'white') + 
+  facet_wrap(~기관명, ncol = 1) +
+  labs(fill = '전체사례수')
+
 
 ggsave("plot2.png", width = 13.5, height = 17.5, units = "cm")
 
@@ -181,14 +186,33 @@ ggsave("plot2.png", width = 13.5, height = 17.5, units = "cm")
 df |> 
   group_by(기관명, 분석분야, 설문대상) |>
   count() |>
-  ungroup()|>
-  ggplot(aes(x = 분석분야, y = 설문대상)) +
-  geom_text(aes(label = n), size = 3) + 
-  facet_wrap(~기관명, ncol = 1)
+  group_by(기관명) |>
+  mutate(nn = sum(n)) |>   ggplot(aes(x = 분석분야, y = 설문대상)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = nn)) +
+  geom_text(aes(label = n), size = 2.5, color = 'white') + 
+  facet_wrap(~기관명, ncol = 1) +
+  labs(fill = '전체사례수')
+
+
+
 
 ggsave("plot3.png", width = 13.5, height = 17.5, units = "cm")
 
 ## 
+df |> 
+  group_by(기관명, 조사방법, 설문대상) |>
+  count() |>
+  group_by(기관명) |>
+  mutate(nn = sum(n)) |>   
+  ggplot(aes(x = 조사방법, y = 설문대상)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = nn)) +
+  geom_text(aes(label = n), size = 2.5, color = 'white') + 
+  facet_wrap(~기관명, ncol = 1) +
+  labs(fill = '전체사례수') +
+  scale_x_discrete(labels = c("만족도조사","면담 및 FGI","설문조사", '패널조사', '협의및토론\n델파이'))
+
+
+
 df |> 
   group_by(기관명, 조사방법, 설문대상) |>
   count() |>
@@ -205,6 +229,19 @@ ggsave("plot4.png", width = 13.5, height = 17.5, units = "cm")
 df |> 
   group_by(기관명, 조사방법, 설문규모) |>
   count() |>
+  group_by(기관명) |>
+  mutate(nn = sum(n)) |>   
+  ggplot(aes(x = 조사방법, y = 설문규모)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = nn)) +
+  geom_text(aes(label = n), size = 2.5, color = 'white') + 
+  facet_wrap(~기관명, ncol = 1) +
+  labs(fill = '전체사례수') +
+  scale_x_discrete(labels = c("만족도조사","면담 및 FGI","설문조사", '패널조사', '협의및토론\n델파이'))
+
+
+df |> 
+  group_by(기관명, 조사방법, 설문규모) |>
+  count() |>
   ungroup()|>
   ggplot(aes(x = 조사방법, y = 설문규모)) +
   geom_text(aes(label = n)) + 
@@ -215,6 +252,18 @@ ggsave("plot5.png", width = 13.5, height = 17.5, units = "cm")
 
 
 ## 
+df |> 
+  group_by(기관명, 설문대상, 설문규모) |>
+  count() |>
+  group_by(기관명) |>
+  mutate(nn = sum(n)) |>   
+  ggplot(aes(x = 설문대상, y = 설문규모)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = nn)) +
+  geom_text(aes(label = n), size = 2.5, color = 'white') + 
+  facet_wrap(~기관명, ncol = 1) +
+  labs(fill = '전체사례수') +
+  scale_x_discrete(labels = c("대국민","정부부처\n교원","연구자\n전문가", '학교행정가', '교원', '학생', '학부모', '전문가\n행정가\n교원', '기타'))
+
 df |> 
   group_by(기관명, 설문대상, 설문규모) |>
   count() |>
@@ -231,6 +280,19 @@ ggsave("plot6.png", width = 13.5, height = 17.5, units = "cm")
 df |> 
   group_by(설문대상, 분석분야, 설문규모) |>
   count() |>
+  group_by(설문대상) |>
+  mutate(nn = sum(n)) |>   
+  ggplot(aes(x = 분석분야, y = 설문규모)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = nn)) +
+  geom_text(aes(label = n), size = 3, color = 'white') + 
+  facet_wrap(~설문대상, ncol = 2) +
+  labs(fill = '전체사례수') +
+  scale_x_discrete(labels = c("교육\n전반","유아\n분야","초중등\n교육", '고등\n교육', '평생\n교육', '기타'))
+
+
+df |> 
+  group_by(설문대상, 분석분야, 설문규모) |>
+  count() |>
   ungroup()|>
   ggplot(aes(x = 분석분야, y = 설문규모)) +
   geom_text(aes(label = n)) + 
@@ -241,6 +303,21 @@ ggsave("plot7.png", width = 13.5, height = 17.5, units = "cm")
 
 
 ## 
+df |> 
+  group_by(설문대상, 분석분야, 조사방법) |>
+  count() |>
+  group_by(설문대상) |>
+  mutate(nn = sum(n)) |>   
+  ggplot(aes(x = 분석분야, y = 조사방법)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = nn)) +
+  geom_text(aes(label = n), size = 3, color = 'white') + 
+  facet_wrap(~설문대상, ncol = 2) +
+  labs(fill = '전체사례수') +
+  scale_x_discrete(labels = c("교육\n전반","유아\n분야","초중등\n교육", '고등\n교육', '평생\n교육', '기타')) +
+  scale_y_discrete(labels = c("만족도조사","면담 및 FGI","설문조사", '패널조사', '협의및토론\n델파이'))
+  
+
+
 df |> 
   group_by(설문대상, 분석분야, 조사방법) |>
   count() |>
@@ -257,6 +334,18 @@ ggsave("plot8.png", width = 13.5, height = 17.5, units = "cm")
 df |> 
   group_by(설문대상, 조사방법, 설문규모) |>
   count() |>
+  group_by(설문대상) |>
+  mutate(nn = sum(n)) |>   
+  ggplot(aes(x = 조사방법, y = 설문규모)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = nn)) +
+  geom_text(aes(label = n), size = 3, color = 'white') + 
+  facet_wrap(~설문대상, ncol = 2) +
+  labs(fill = '전체사례수') +
+  scale_x_discrete(labels = c("만족도조사","면담\nFGI","설문조사", '패널조사', '협의및토론\n델파이'))
+
+df |> 
+  group_by(설문대상, 조사방법, 설문규모) |>
+  count() |>
   ungroup()|>
   ggplot(aes(x = 조사방법, y = 설문규모)) +
   geom_text(aes(label = n)) + 
@@ -264,3 +353,78 @@ df |>
   scale_x_discrete(labels = c("만족도조사","면담\nFGI","설문조사", '패널조사', '협의및토론\n델파이'))
 
 ggsave("plot9.png", width = 13.5, height = 17.5, units = "cm")
+
+##################################
+
+df |> 
+  group_by(기관명, 분석분야) |>
+  count() |>
+  ggplot(aes(x = 기관명, y = n, fill = 분석분야)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = -0, hjust = 0.5), 
+        legend.position = 'bottom') + 
+  labs(y = '사례수')
+
+ggsave("연구plot1.png", width = 13.5, height = 10, units = "cm")
+
+##################################
+
+df |> 
+  group_by(기관명, 조사방법) |>
+  count() |>
+  ggplot(aes(x = 기관명, y = n, fill = 조사방법)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = -0, hjust = 0.5), 
+        legend.position = 'bottom') + 
+  labs(y = '사례수') + 
+  guides(fill = guide_legend(nrow = 2))
+
+ggsave("연구plot2.png", width = 13.5, height = 10, units = "cm")
+
+
+
+##################################
+
+df |> 
+  group_by(기관명, 분석대상) |>
+  count() |>
+  ggplot(aes(x = 기관명, y = n, fill = 분석대상)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = -0, hjust = 0.5), 
+        legend.position = 'bottom') + 
+  labs(y = '사례수') + 
+  guides(fill = guide_legend(nrow = 1))
+
+ggsave("연구plot3.png", width = 13.5, height = 10, units = "cm")
+
+
+##################################
+
+df |> 
+  group_by(기관명, 설문대상) |>
+  count() |>
+  ggplot(aes(x = 기관명, y = n, fill = 설문대상)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = -0, hjust = 0.5), 
+        legend.position = 'bottom') + 
+  labs(y = '사례수') + 
+  guides(fill = guide_legend(nrow = 3))
+
+ggsave("연구plot4.png", width = 13.5, height = 10, units = "cm")
+
+
+
+##################################
+
+df |> 
+  group_by(기관명, 설문규모) |>
+  count() |>
+  ggplot(aes(x = 기관명, y = n, fill = 설문규모)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = -0, hjust = 0.5), 
+        legend.position = 'bottom') + 
+  labs(y = '사례수') + 
+  guides(fill = guide_legend(nrow = 1))
+
+ggsave("연구plot5.png", width = 13.5, height = 10, units = "cm")
+
